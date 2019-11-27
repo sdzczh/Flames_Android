3ad509048b92fa2a0edd2ef09cfb26922a36024e
whe0804 <935317231@qq.com>
2019-11-27 21:48:16 +0800

    test

--- app/src/main/java/app/com/pgy/Adapters/MortgageorderAdapter.java
+++ app/src/main/java/app/com/pgy/Adapters/MortgageorderAdapter.java
@@ -7,6 +7,7 @@ import android.support.annotation.Nullable;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import app.com.pgy.Models.Beans.MortgageorderBean;
@@ -24,12 +25,16 @@ public class MortgageorderAdapter extends BaseQuickAdapter<MortgageorderBean, Ba
         helper.setText(R.id.tv_diyaorder_num,item.getAmount()+"PGY");
         helper.setText(R.id.tv_diyaorder_diyadate,"抵押日期："+item.getStartTime());
         helper.setText(R.id.tv_diyaorder_releasedate,"释放日期："+item.getEndTime());
-        helper.setText(R.id.tv_diyaorder_rate,"日利率："+item.getRate()*100+"%");
+        double mul = mul(item.getRate(), 100.0);
+        helper.setText(R.id.tv_diyaorder_rate,"日利率："+mul+"%");
         TextProgressBar progressbar = (TextProgressBar) helper.itemView.findViewById(R.id.progressbar);
         progressbar.setProgress(Double.valueOf(item.getPercentage()).intValue());
-//        progressbar.setProgress(Double.valueOf(12.6).intValue());
-//        progressbar.setState(102);
-//        progressbar.setProgress(50);
 
+    }
+
+    public static double mul(double v1,double v2){
+        BigDecimal b1 = new BigDecimal(Double.toString(v1));
+        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        return b1.multiply(b2).doubleValue();
     }
 }
